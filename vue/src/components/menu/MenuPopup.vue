b7253c7235834ebe6f040656f2156087b27383f0
huy1235588 <viettien4869@gmail.com>
2024-08-29 18:37:55 +0700

    fix: update toggle button MenuPopup

--- vue/src/components/menu/MenuPopup.vue
+++ vue/src/components/menu/MenuPopup.vue
@@ -1,20 +1,34 @@
 <template>
-    <aside v-if="isVisible" class="menu-popup-container" :style="popupStyle">
+    <aside
+        id="menu-popup"
+        v-if="isVisible"
+        class="menu-popup-container"
+        :style="popupStyle"
+    >
         <ul id="items">
             <!-- Delete video -->
             <li class="menu-item">
                 <div class="menu-item-container">
                     <span id="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24"
-                            viewBox="0 0 24 24" width="24" focusable="false" aria-hidden="true" style="
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            enable-background="new 0 0 24 24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            focusable="false"
+                            aria-hidden="true"
+                            style="
                                 pointer-events: none;
                                 display: inherit;
                                 width: 100%;
                                 height: 100%;
                                 fill: currentColor;
-                            ">
-                            <path d="M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z">
-                            </path>
+                            "
+                        >
+                            <path
+                                d="M11 17H9V8h2v9zm4-9h-2v9h2V8zm4-4v1h-1v16H6V5H5V4h4V3h6v1h4zm-2 1H7v15h10V5z"
+                            ></path>
                         </svg>
                     </span>
                     <div id="menu-service">
@@ -27,17 +41,24 @@
             <li class="menu-item" has-separator>
                 <div class="menu-item-container">
                     <span id="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"
-                            focusable="false" aria-hidden="true" style="
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            focusable="false"
+                            aria-hidden="true"
+                            style="
                                 pointer-events: none;
                                 display: inherit;
                                 width: 100%;
                                 height: 100%;
                                 fill: currentColor;
-                            ">
+                            "
+                        >
                             <path
-                                d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z">
-                            </path>
+                                d="M15 5.63 20.66 12 15 18.37V14h-1c-3.96 0-7.14 1-9.75 3.09 1.84-4.07 5.11-6.4 9.89-7.1l.86-.13V5.63M14 3v6C6.22 10.13 3.11 15.33 2 21c2.78-3.97 6.44-6 12-6v6l8-9-8-9z"
+                            ></path>
                         </svg>
                     </span>
                     <div id="menu-service">
@@ -49,17 +70,24 @@
             <li class="menu-item">
                 <div class="menu-item-container">
                     <span id="icon">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"
-                            focusable="false" aria-hidden="true" style="
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            width="24"
+                            focusable="false"
+                            aria-hidden="true"
+                            style="
                                 pointer-events: none;
                                 display: inherit;
                                 width: 100%;
                                 height: 100%;
                                 fill: currentColor;
-                            ">
+                            "
+                        >
                             <path
-                                d="M14.04 13.61 16.86 17H11.5l.3-.4 2.24-2.99m-5.11 1.08 1.24 1.86.3.45H7.08l1.85-2.31M14 12l-3 4-2-3-4 5h14l-5-6zm6-8v16H4V4h16m1-1H3v18h18V3z">
-                            </path>
+                                d="M14.04 13.61 16.86 17H11.5l.3-.4 2.24-2.99m-5.11 1.08 1.24 1.86.3.45H7.08l1.85-2.31M14 12l-3 4-2-3-4 5h14l-5-6zm6-8v16H4V4h16m1-1H3v18h18V3z"
+                            ></path>
                         </svg>
                     </span>
                     <div id="menu-service">
@@ -76,44 +104,44 @@ import { nextTick, onMounted, onUnmounted, ref } from 'vue';
 import emitter from '../../eventBus';
 
 const isVisible = ref(false); // Check MenuPopup ẩn hay hiện
-const isShowPopup = ref(true);
 const popupStyle = ref({}); // Sử dụng object để điều chỉnh vị trí của popup
+const pressedButton = ref(""); // Lưu lại nút đã nhấn
 
 // Hàm hiện menu popup
-const showPopup = (event) => {
-    isVisible.value = isShowPopup.value;
+const showPopup = (payload) => {
+    const elementTarget = payload.event.currentTarget.id;
 
-    // Vị trí hiện menu
-    nextTick(() => {
-        const targetElement = event.event.target; // Element được nhấn để hiển thị popup
-        const rect = targetElement.getBoundingClientRect();
+    // Kiểm tra popup chưa hiển thị
+    if (elementTarget.startsWith("button-")) {
+        // Kiểm tra nếu cùng một nút đã được nhấn
+        if (elementTarget === pressedButton.value) {
+            isVisible.value = !isVisible.value; // Chuyển đổi trạng thái
+        } else {
+            isVisible.value = true; // Hiển thị popup
+            pressedButton.value = elementTarget; // Lưu lại ID Element của nút đã được nhấn
+        }
 
-        popupStyle.value = {
-            top: `${rect.bottom}px`, // Cộng với vị trí cuộn
-            left: `${rect.left - 365}px`, // Cộng với vị trí cuộn và 50px
-        };
-    });
+        // Vị trí hiện menu
+        nextTick(() => {
+            const targetElement = payload.event.target;
+            const rect = targetElement.getBoundingClientRect();
+            popupStyle.value = {
+                top: `${rect.bottom}px`,
+                left: `${rect.left - 365}px`,
+            };
+        });
 
-    //  Ngăn chặn việc hidePopup được gọi ngay lập tức
-    setTimeout(() => {
-        // Check nếu popup đã hiện
-        if (isVisible.value) {
-            document.addEventListener('click', hidePopup);
-        };
-    }, 0);
-
-    isShowPopup.value = !isShowPopup.value;
+        // Thêm sự kiện click để ẩn popup
+        document.addEventListener('click', hidePopup);
+    }
 }
 
 // Hàm ẩn menu popup
 const hidePopup = (event) => {
-    const button = document.querySelector('button[id^="button-"]');
-
-    if (!button.contains(event.target) && event.target !== button) {
+    if (!event.target.closest('#menu-popup') && !event.target.closest('button[id^="button-"]')) {
         isVisible.value = false;
-        isShowPopup.value = !isShowPopup.value;
+        document.removeEventListener('click', hidePopup);
     }
-    document.removeEventListener('click', hidePopup);
 }
 
 onMounted(() => {
