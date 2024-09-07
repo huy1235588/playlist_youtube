469b67e082aa8ea514d478da33589a48d1f6a7fc
huy1235588 <viettien4869@gmail.com>
2024-09-07 21:55:51 +0700

    end project

--- backend/src/routes/videoRoutes.js
+++ backend/src/routes/videoRoutes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/videoController');
-const replaceVideoController = require('../controllers/replaceVideoController')
 
 // Route để lấy tất cả các video từ youtube api
 router.get('/video/fetch', videoController.fetchAndSaveVideos);
-// Route để thay thể video trong playlist
-router.post('/video/replace', replaceVideoController.replaceVideoInPlaylist);
 
 module.exports = router;
