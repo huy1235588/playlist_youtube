c04c35a79c6ab63e173c48ec8c8c3734f487a822
huy1235588 <viettien4869@gmail.com>
2024-09-08 16:47:26 +0700

    add route delete video

--- backend/src/routes/videoRoutes.js
+++ backend/src/routes/videoRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const videoController = require('../controllers/videoController');
+const deleteController = require('../controllers/videoActionController');
 
 // Route để lấy tất cả các video từ youtube api
 router.get('/video/fetch', videoController.fetchAndSaveVideos);
+
+// Route để xóa video trong database
+router.get('/video/delete', deleteController.deleteVideo);
 
 module.exports = router;
